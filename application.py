475bd6b198ade1a14158869a5624cce2141410af
LeonardoMSA <leonardomsazevedo@gmail.com>
2023-06-16 16:45:33 -0300

    Starting to add database functinalities

--- application.py
+++ application.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 application = Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+db = SQLAlchemy(application)
+
+class Todo(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(500), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @application.route('/')
 
 def index():
-    return "Hello world"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     application.run(debug=True)
